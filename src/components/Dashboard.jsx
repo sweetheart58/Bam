5ca709be8113c88764ab4ccd6956e0d6aad255d5
sweetheart <sweetheart199558@gmail.com>
2020-11-25 14:08:25 +0530

    Basic Setup

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
+import { Context } from "../store/Context";
+import { useHistory } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [state, dispatch] = useContext(Context);
+  const [isAuth, setAuth] = useState(false);
+  const history = useHistory();
+  useEffect(() => {
+    if (state === undefined) {
+      history.push("/");
+    } else {
+      setAuth(true);
+    }
+  }, [history, state]);
+  if (isAuth) {
+    return <div>Hi {state.username}</div>;
+  } else {
+    return <div>Please login with your google account</div>;
+  }
 };
 
 export default Dashboard;
